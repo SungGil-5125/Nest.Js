44988a2ecd8fcd4303168f47bf96f84ca2068c95
SungGil-5125 <sunggil0125@naver.com>
2022-03-12 13:30:17 +0900

    Update movies.service.ts

--- hi-nest/src/movies/movies.service.ts
+++ hi-nest/src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,10 +10,17 @@ export class MoviesService {
     }
 
     getOne(id : string) : Movie {
-        return this.movies.find(movie => movie.id === +id);
+        const movie = this.movies.find(movie => movie.id === +id);
+
+        if(!movie){
+            throw new NotFoundException(`movie with id ${id} not found`)
+        }
+
+        return movie;
     }
     
     deleteOne(id : string): boolean {
+        this.getOne(id);
         this.movies.filter(movie => movie.id !== +id);
         return true;
     }
